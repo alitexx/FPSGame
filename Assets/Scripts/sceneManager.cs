79270aac8401f5ccd14c3f0846884a5763204952
Katie T <kttackett123@gmail.com>
2023-04-17 12:04:45 -0400

    created win/lose/pause menus

---
+++ Assets/Scripts/sceneManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class sceneManager : MonoBehaviour
+{
+
+    public void endGame()
+    {
+        Application.Quit();
+    }
+    public void loadLevel(string sceneName)
+    {
+        Debug.Log("Changing Scenes!");
+        Time.timeScale = 1f;
+        if (sceneName == "Game")
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        } else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        SceneManager.LoadScene(sceneName);
+    }
+}
