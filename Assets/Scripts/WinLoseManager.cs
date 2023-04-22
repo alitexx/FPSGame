3aa8b819cf69c5d4dcc714f71ec1fce18ab23a4b
Katie T <kttackett123@gmail.com>
2023-04-22 16:32:15 -0400

    display information on game end

--- Assets/Scripts/WinLoseManager.cs
+++ Assets/Scripts/WinLoseManager.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class WinLoseManager : MonoBehaviour
 {
-    
+    public TextMeshProUGUI defeatedTXT;
+
+    private void Start()
+    {
+        if (gameObject.name == "LoseScreen")
+        {
+            defeatedTXT.text = "Time Survived: " + (60 - Mathf.Round(globalVariables.timer)).ToString() + "\nBears Defeated: " + (globalVariables.bearsKilled).ToString();
+        } else if (gameObject.name == "PauseMenu")
+        {
+            defeatedTXT.text = "Time Remaining: " + (Mathf.Round(globalVariables.timer)).ToString() + "\nBears Defeated: " + (globalVariables.bearsKilled).ToString();
+        }
+        else
+        {
+            defeatedTXT.text = "Bears Defeated: " + (globalVariables.bearsKilled).ToString();
+        }
+        
+    }
 }
